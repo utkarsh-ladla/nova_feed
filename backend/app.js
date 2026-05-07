6c61cfb5b532754f218f1ebb849c7911527eb873
UTKARSH LADLA <128512468+utkarsh-ladla@users.noreply.github.com>
2026-05-07 16:36:14 +0530

    feat: initialize backend project with Express server, project dependencies, and authentication routes

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const cors = require('cors')
 const connectDB = require('./config/db')
+const morgan = require('morgan')
+
+const authRoutes = require('./routes/authRoutes');
+
+const {
+    notFound,
+    errorHandler,
+} = require("./middleware/errorMiddleware");
 
 const app = express();
 
 app.use(cors())
 app.use(express.json());
-
+app.use(morgan("dev"));
 app.get("/", (req, res) => {
     res.send("NovaFeed Backend API is running...")
 
 })
+app.use('/api/auth', authRoutes);
+app.use(notFound);
+app.use(errorHandler);
+
 module.exports = app;
